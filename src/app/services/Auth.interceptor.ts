8e88307dd949331165102386f1223839509707b1
Earthii <xiao_eric@hotmail.com>
2018-09-22 18:42:10 -0400

    Added check in Auth Http Interceptor to check for github requests

--- src/app/services/Auth.interceptor.ts
+++ src/app/services/Auth.interceptor.ts
@@ -10,18 +10,20 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  accessToken = '5ff4f075f2d2e3f018c98834a552c79426373fde';
+  githubAccessToken = '5ff4f075f2d2e3f018c98834a552c79426373fde';
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    req = req.clone({
-      setHeaders: {
-        'Content-Type': 'application/json; charset=utf-8',
-        Accept: 'application/json',
-        Authorization: `Bearer ${this.accessToken}`
-      }
-    });
+    if (req.url.includes('github')) {
+      req = req.clone({
+        setHeaders: {
+          'Content-Type': 'application/json; charset=utf-8',
+          Accept: 'application/json',
+          Authorization: `Bearer ${this.githubAccessToken}`
+        }
+      });
+    }
 
     return next.handle(req);
   }
